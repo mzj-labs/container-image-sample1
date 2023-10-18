af275c81fc9870a34c71d3b6d44491566d8c4f17
habuka036 <osamu@habuka.jp>
2023-10-18 21:26:56 +0900

    add buildspec.docker-push.sh

---
+++ buildspec.docker-push.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$CODEBUILD_WEBHOOK_HEAD_REF" ]; then
+  export IMAGE_TAG=`echo $CODEBUILD_WEBHOOK_HEAD_REF | rev | cut -d'/' -f1 | rev`
+fi
+if [ -z "$IMAGE_TAG" ]; then
+  export IMAGE_TAG="latest"
+fi
+echo docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
